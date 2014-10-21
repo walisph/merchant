0c7445078630ffa4a0e49b4803d3ee78f0942833
jayaregalinada <jayaregalinada@gmail.com>
2014-10-22 00:22:23 +0800

    Fixed indentation and remove other interface

--- src/Walisph/Merchant/ProductInterface.php
+++ src/Walisph/Merchant/ProductInterface.php
@@ -2,14 +2,12 @@
 
 interface ProductInterface {
 
-    public function getDataAttribute( $value );
-    public function getImagesAttribute( $value );
-    public function getProductName();
-    public function getDescription();
-    public function getPrice();
-    public function getManufacturer();
-    public function getBrand();
-    public function getProductModel();
+	public function getProductName();
+	public function getDescription();
+	public function getPrice();
+	public function getManufacturer();
+	public function getBrand();
+	public function getProductModel();
 
 }
  
