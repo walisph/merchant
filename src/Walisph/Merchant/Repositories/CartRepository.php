374d967f8ec0bed803bd6e6bc25c243dca3335ea
jayaregalinada <jayaregalinada@gmail.com>
2014-10-22 14:30:14 +0800

    Create a CartController

--- src/Walisph/Merchant/Repositories/CartRepository.php
+++ src/Walisph/Merchant/Repositories/CartRepository.php
@@ -1,9 +1,38 @@
 <?php namespace Walisph\Merchant\Repositories;
 
 use BaseController;
+use Illuminate\Support\Facades\App;
+use Walisph\Merchant\Eloquent\InvoiceRepository;
+use Walisph\Merchant\Eloquent\ProductRepository;
 
 class CartRepository extends BaseController {
+	
+	protected function createInvoice( $view, $data = [] )
+	{
+		$brochure = App::make('walis-merchant.brochure');
+		$invoice = InvoiceRepository::create([
+			'product_id' => $data['product'],
+			'quantity' => $data['quantity'],
+			'customer_email' => $data['customer']['email'],
+			'customer_data' => $data['customer'],
+			'product_data' => ProductRepository::find( $data['product'] )->toArray()
+		]);
+		$brochure->loadView( $view, $invoice );
+		$filename = 'invoice_'. $invoice->id . date( "Y-n-d-His" ) .'.pdf';
+		$brochure->save( public_path( 'invoices/'. $filename ) );
+		return [
+			'file_name' => $filename,
+			'file_path' => 'invoices/'. $filename,
+			'invoice_id' => $invoice->id
+		];
+	}
 
-
+	public function downloadInvoice( $view, $id )
+	{
+		$brochure = App::make('walis-merchant.brochure');
+		$brochure->loadView( $view, InvoiceRepository::find( $id ) );
+		$filename = 'invoice_'. sha1( date( "Y-n-d-His" ) ) .'.pdf';
+		return $brochure->download( $filename );
+	}
 }
  
