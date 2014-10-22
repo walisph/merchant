4b9f683417ac302a2537f76d9e5736253146133c
jayaregalinada <jayaregalinada@gmail.com>
2014-10-22 14:29:35 +0800

    Create a Invoice Model

---
+++ src/Walisph/Merchant/Eloquent/InvoiceRepository.php
@@ -0,0 +1,29 @@
+<?php namespace Walisph\Merchant\Eloquent;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class InvoiceRepository extends Eloquent {
+
+	protected $table = 'invoices';
+	protected $fillable = [ 'product_id', 'quantity', 'customer_email', 'customer_data', 'product_data' ];
+
+	public function setCustomerDataAttribute( $value )
+	{
+		return $this->attributes['customer_data'] = serialize( $value );
+	}
+
+	public function setProductDataAttribute( $value )
+	{
+		return $this->attributes['product_data'] = serialize( $value );
+	}
+
+	public function getCustomerDataAttribute( $value )
+	{
+		return unserialize( $value );
+	}
+
+	public function getProductDataAttribute( $value )
+	{
+		return unserialize( $value );
+	}
+}
