3a30cab89d93d4a181335d1292da204d0b1c323a
jayaregalinada <jayaregalinada@gmail.com>
2014-10-22 14:29:05 +0800

    Create a Invoice migration

---
+++ src/migrations/2014_10_19_233702_create_invoices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvoicesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('invoices', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('product_id');
+			$table->integer('quantity');
+			$table->text('customer_email');
+			$table->text('customer_data');
+			$table->text('product_data');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('invoices');
+	}
+
+}
